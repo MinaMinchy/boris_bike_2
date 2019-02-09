4ae5924f4ce34415bb00e7a7ed6a18865b7b7ad9
Amina Jusic <amina_jusic@yahoo.com>
2019-02-09 23:22:38 +0000

    Step 14 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,4 +1,4 @@
-require_relative 'bike'
+# require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
@@ -9,13 +9,13 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' unless !@bikes.empty?  #'guard condition'
+    fail 'No bikes available' if @bikes.empty?  #'guard condition'
     @bikes.pop
   end
 
   def dock_bike(bike)
-    raise "There is no more capacity" unless @bikes.length == 0
-     @bikes << bike
+    raise "There is no more capacity" if @bikes.count >=20 #error if bike is already has been docked
+     @bikes << bike #saves docked bike into memory so we can see it later
 
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,10 +10,8 @@ describe DockingStation do
     bike = Bike.new
     docking_station.dock_bike(bike)
     expect(docking_station.release_bike.working?).to eq true
-
-
-
   end
+
 
   it 'docks a bike' do
     # arrange
@@ -35,13 +33,19 @@ describe DockingStation do
   end
 
 
-  it 'return error if its docking capacity is exceeded ' do
-     docking_station = DockingStation.new
-     bike = Bike.new
-     docking_station.dock_bike(bike)
-     bike2 = Bike.new
-     expect { docking_station.dock_bike(bike2)}.to raise_error "There is no more capacity"
+  # it 'return error if its docking capacity is exceeded ' do
+  #    docking_station = DockingStation.new
+  #    bike = Bike.new
+  #    docking_station.dock_bike(bike)
+  #    bike2 = Bike.new
+  #    expect { docking_station.dock_bike(bike2)}.to raise_error "There is no more capacity"
 
+
+  it 'raises error if docking station at capacity' do
+    docking_station = DockingStation.new
+    bike = Bike.new
+    20.times { docking_station.dock_bike Bike.new }
+    expect { docking_station.dock_bike(Bike.new)}.to raise_error{'No spase left'}
   end
 
 end
